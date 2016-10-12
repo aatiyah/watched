43bb376bab71550810da32e952bdb381ec6c07d8
aatiyah <medatiyah@gmail.com>
2016-10-12 08:52:41 -0500

    Added .includes to user_controller to avoid n+1, and reflected changes in users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-  	@seasons = Season.all.order("number")
+  	@seasons = Season.all.order("number").includes(:episodes)
   end
 end
